a76f89a995e0ad08c3f21cb34cd06d4719d91e9c
jizya0208 <moridyan606@gmail.com>
2022-03-11 22:56:16 +0900

    add controller

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// レストラン一覧画面を表示
+Route::get('/', 'RestaurantController@showList')->name('restaurants');
+Route::get('/reviews', 'ReviewtController@create')->name('restaurants');
